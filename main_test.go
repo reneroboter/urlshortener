50aa03b0e040502c9b0a2ef0001d399af64e3ee6
René Backhaus <rene.backhaus@real-digital.de>
2025-11-26 18:24:13 +0100

    Add some more tests

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -51,3 +52,41 @@ func Test_GetRequestHandler_ReturnsRedirect(t *testing.T) {
 		t.Errorf("expected 301, got %d", rr.Code)
 	}
 }
+
+func Test_PostRequestHandler_ReturnsId(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /shorten", PostRequestHandler)
+
+	req := httptest.NewRequest(http.MethodPost, "/shorten", bytes.NewBuffer([]byte(`{"url":"http://www.google.com"}`)))
+	rr := httptest.NewRecorder()
+
+	mux.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("expected 200, got %d", rr.Code)
+	}
+
+	if !strings.ContainsAny("738ddf35b3a85a7a6ba7b232bd3d5f1e4d284ad1", rr.Body.String()) {
+		t.Errorf("expected '738ddf35b3a85a7a6ba7b232bd3d5f1e4d284ad1', got '%s'", rr.Body.String())
+	}
+}
+
+func Test_PostRequestHandler_BadRequest(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /shorten", PostRequestHandler)
+
+	req := httptest.NewRequest(http.MethodPost, "/shorten", bytes.NewBuffer([]byte(`{"url":"www.google.com"}`)))
+	rr := httptest.NewRecorder()
+
+	mux.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusBadRequest {
+		t.Errorf("expected 400, got %d", rr.Code)
+	}
+
+	if strings.TrimSpace(rr.Body.String()) != "invalid URL format" {
+		t.Errorf("expected 'invalid URL format', got '%s'", rr.Body.String())
+	}
+}
+
+// 1. `http POST localhost:8888/shorten url=http://www.google.de`-> (Second post) Returns bad request error
