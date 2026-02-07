d609cc5d34733045e8fe62499ec4e23bbcd0c0a6
René Backhaus <rene.backhaus@real-digital.de>
2026-02-07 10:39:58 +0100

    add store idea

---
+++ store.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/sha1"
+	"errors"
+	"fmt"
+)
+
+type StoreInterface interface {
+	Put(url string) (string, error)
+	Get(code string) (string, error)
+}
+type InMemoryStore struct {
+	m map[string]string
+}
+
+type FileStore struct {
+	m map[string]string
+}
+
+func NewInMemoryStore() *InMemoryStore {
+	// todo how to ensure singleton?
+	return &InMemoryStore{m: make(map[string]string)}
+}
+
+func (s *InMemoryStore) Put(url string) (string, error) {
+	code := fmt.Sprintf("%x", sha1.Sum([]byte(url)))
+	s.m[code] = url
+	return code, nil
+}
+
+func (s *InMemoryStore) Get(code string) (string, error) {
+	url, ok := s.m[code]
+	if !ok {
+		return "", errors.New("not found")
+	}
+	return url, nil
+}
