dabba16e7179c19065c46c061197dfcb5056a0ed
René Backhaus <rene.backhaus@real-digital.de>
2026-02-07 23:31:12 +0100

    add envs for redis config

--- pkg/redis/client.go
+++ pkg/redis/client.go
@@ -5,9 +5,10 @@ import (
 )
 
 func NewRedisClient() *redis.Client {
+	config := NewRedisConfig()
 	return redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "",
-		DB:       0,
+		Addr:     config.addr,
+		Password: config.passwd,
+		DB:       config.db,
 	})
 }
--- pkg/redis/config.go
+++ pkg/redis/config.go
@@ -1,1 +1,18 @@
 package redis
+
+import "os"
+
+type redisConfig struct {
+	addr   string
+	passwd string
+	db     int
+}
+
+func NewRedisConfig() *redisConfig {
+
+	return &redisConfig{
+		addr:   os.Getenv("REDIS_HOST"),
+		passwd: os.Getenv("REDIS_PASSWORD"),
+		db:     0,
+	}
+}
