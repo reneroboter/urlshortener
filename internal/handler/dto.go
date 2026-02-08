1946fd00f7a2ec3fb16a17394cd704b24934b97f
René Backhaus <rene.backhaus@real-digital.de>
2026-02-08 15:23:24 +0100

    add simple kafka impression tracking

--- internal/handler/dto.go
+++ internal/handler/dto.go
@@ -7,3 +7,11 @@ type PostRequest struct {
 type PostResponse struct {
 	Code string `json:"code"`
 }
+
+type KafkaEvent struct {
+	EventID    string `json:"eventId"`
+	Code       string `json:"code"`
+	URL        string `json:"url"`
+	OccurredAt int64  `json:"occurredAt"`
+	Type       string `json:"type"`
+}
