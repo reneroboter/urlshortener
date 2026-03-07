58b08b755e4071490e60cac0ee2517301f92daff
René Backhaus <rene.backhaus@real-digital.de>
2026-03-07 15:19:27 +0100

    fix build issues

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,8 @@ import (
 	"net/http"
 
 	"github.com/reneroboter/urlshortener/internal/application"
-	"github.com/reneroboter/urlshortener/internal/infrastructure"
 	"github.com/reneroboter/urlshortener/internal/interfaces"
 )
-
-var Store = infrastructure.NewShortUrlRepository()
 
 func main() {
 
@@ -20,7 +17,7 @@ func main() {
 	mux.HandleFunc("POST /shorten", interfaces.PostCreateShortURLHandler(shortURLService))
 	mux.HandleFunc("GET /{code}", interfaces.GetRequestHandler(shortURLService))
 
-	err = http.ListenAndServe(":8888", mux)
+	err := http.ListenAndServe(":8888", mux)
 	if err != nil {
 		slog.Error("Something went wrong", "err", err)
 		return
