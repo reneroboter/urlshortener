d133bedfbb11a1a9601e43e3ed188a1d09af71a6
René Backhaus <rene.backhaus@real-digital.de>
2026-03-07 16:09:02 +0100

    cleanup

--- internal/interfaces/dto.go
+++ internal/interfaces/dto.go
@@ -23,11 +23,3 @@ type GetShortURLRequest struct {
 func (r GetShortURLRequest) Validate() bool {
 	return domain.IsValidCode(r.Code)
 }
-
-type KafkaEvent struct {
-	EventID    string `json:"eventId"`
-	Code       string `json:"code"`
-	URL        string `json:"url"`
-	OccurredAt int64  `json:"occurredAt"`
-	Type       string `json:"type"`
-}
--- pkg/kafka/client.go
+++
@@ -1,9 +0,0 @@
-package kafka
-
-import (
-	"github.com/twmb/franz-go/pkg/kgo"
-)
-
-func NewKafkaClient() (*kgo.Client, error) {
-	return kgo.NewClient(NewKafkaConfig()...)
-}
