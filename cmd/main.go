26e472aa8f92cfff4a12ca9e626015198cda513d
René Backhaus <rene.backhaus@real-digital.de>
2026-03-30 21:44:19 +0200

    playaround with fx

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,59 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log/slog"
+	"net"
 	"net/http"
+	"os"
 
 	"github.com/reneroboter/urlshortener/internal/application"
 	"github.com/reneroboter/urlshortener/internal/interfaces"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
 )
 
-func main() {
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8888", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Starting HTTP server at", srv.Addr)
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
 
+func NewServeMux() *http.ServeMux {
 	shortURLService := application.NewShortURLService()
-	slog.Info("Start urlshortener")
 	mux := http.NewServeMux()
-
 	mux.HandleFunc("POST /shorten", interfaces.PostCreateShortURLHandler(shortURLService))
 	mux.HandleFunc("GET /{code}", interfaces.GetRequestHandler(shortURLService))
+	return mux
+}
 
-	err := http.ListenAndServe(":8888", mux)
-	if err != nil {
-		slog.Error("Something went wrong", "err", err)
-		return
-	}
+func NewLogger() *slog.Logger {
+	return slog.New(slog.NewTextHandler(os.Stdout, nil))
+}
+
+func main() {
+	slog.Info("Start urlshortener")
+	fx.New(
+		fx.WithLogger(func(logger *slog.Logger) fxevent.Logger {
+			return &fxevent.ConsoleLogger{W: os.Stdout}
+		}),
+		fx.Provide(NewLogger),
+		fx.Provide(NewHTTPServer),
+		fx.Provide(NewServeMux),
+		fx.Invoke(func(*http.Server) {}),
+	).Run()
 }
--- go.mod
+++ go.mod
@@ -11,4 +11,9 @@ require (
 	github.com/klauspost/compress v1.18.4 // indirect
 	github.com/pierrec/lz4/v4 v4.1.25 // indirect
 	github.com/twmb/franz-go/pkg/kmsg v1.12.0 // indirect
+	go.uber.org/dig v1.19.0 // indirect
+	go.uber.org/fx v1.24.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 )
